40d36ce913d49d128ddbb9356c4d3b4e6c2b8dc4
Rahy-Sam <raymond.mwaiki@student.moringaschool.com>
2023-12-08 09:56:14 -0800

    -conditional-statements labs

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,57 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+        def admin_login(username, password):
+    if username == "zaki" and password == 1234:
+        print("Access granted")
+    else:
+        print("Not granted")
 
-def hows_the_weather(temperature):
-    # your code here
+
+admin_login("zaki", 1234)
     pass
+    def hows_the_weather(temperature):
+    if temperature > 50:
+        print("Use an umbrella")
+    else:
+        
+hows_the_weather()
+
 
 def fizzbuzz(num):
+      if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+
+
+result = fizzbuzz(15)
+print(result)
     # your code here
     pass
 
 def calculator(operation, num1, num2):
+    def calculator(operation, num1, num2):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Cannot divide by zero"
+    else:
+        return "Invalid operation"
+
+result = calculator("add", 5, 3)
+print(result)
+
     # your code here
     pass
---
+++ lib/try.py
@@ -0,0 +1,19 @@
+# secret_word = "zaki"
+
+# guess = ""
+
+# while secret_word != zaki:
+#     guess = input("enter a guesss:")
+#     print(ivalid)
+#     secret_word = zaki:
+#     print(congrats)
+
+
+secret_word = "zaki"
+
+guess = ""
+
+while guess != secret_word:
+    guess = input("Enter a guess: ")
+    if guess == secret_word:
+        print("win")
